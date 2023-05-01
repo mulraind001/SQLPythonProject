1c9495fa734f92559c50ddaa233b39aade059130
Denise Buckhana <57542774+mulraind001@users.noreply.github.com>
2023-05-01 17:23:31 -0400

    THe project is done. Added the comments and checked the connection.

--- main.py
+++ main.py
@@ -12,83 +12,47 @@ mydb = mysql.connector.connect(
     port='3306',
     database='python_sql'
 )
+#This is a cursor that points to the database
+myCursor = mydb.cursor()
 
-mycursor = mydb.cursor()
-
-mycursor.execute('SELECT * FROM users')
-
-users = mycursor.fetchall()
-
+#This is make sure the SQL Connection is working. It will show the table in the console
+myCursor.execute('SELECT * FROM users')
+#Fetch all from the table users.
+users = myCursor.fetchall()
+#Print out the table.
 for user in users:
     print(user)
 
 @app.route('/')
-def hello_world():
+def homepage():
     return render_template("login.html")
-database={'admin': '123'}
-
 
 @app.route('/login', methods = ['POST','GET'])
 def login():
-    name1 = request.form['username']
-    pwd = request.form['password']
-    if name1 not in database:
-        return render_template('login.html', info='Invalid User')
-    else:
-        if database[name1] != pwd:
-            return render_template('login.html', info='Invalid Password')
+    #Check if account exists using MySQL
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+
+        #Comment out the following code that you do not want to test. Be sure comment one or the other, not both.
+        #Bad example with no input validation.  Here we pass the user's input raw.
+        #myCursor.execute("SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'")
+
+        # Correct way using parameters.  Here we sanitize the user's input.
+        myCursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
+
+        #Fetch one record and return result
+        account = myCursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Redirect to home page
+            return render_template('home.html', name=username)
         else:
-            return render_template('home.html', name=name1)
+            # Account doesn't exist or username/password incorrect
+            return render_template('login.html', info='Incorrect Credentials')
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
 
 
 if __name__ == '__main__':
     app.run()
-
-#queryResults = mycursor.execute('SELECT * FROM users WHERE username = "' + name1 + '"')
-    #
-    # if queryResults != 1: print("fail")
-    # else: print("pass")
-
-
-
-# # Create a custom request handler
-# class MyRequestHandler(BaseHTTPRequestHandler):
-#
-#     # Handle GET requests
-#     def do_GET(self):
-#         # Send response status code
-#         self.send_response(200)
-#
-#         # Send headers
-#         self.send_header('Content-type', 'text/html')
-#         self.end_headers()
-#
-#         # Send HTML response
-#         html = """
-#         <!DOCTYPE html>
-#         <html>
-#         <head>
-#             <title>Username and Password Credentials</title>
-#         </head>
-#         <body>
-#             <h1>Username and Password Credentials</h1>
-#             <form>
-#                 <label for="username">Username:</label>
-#                 <input type="text" id="username" name="username" value="Aggies23"><br><br>
-#                 <label for="password">Password:</label>
-#                 <input type="password" id="password" name="password" value="AggiePride23"><br><br>
-#                 <input type="submit" value="Submit">
-#             </form>
-#         </body>
-#         </html>
-#         """
-#         self.wfile.write(html.encode())
-#
-#
-# # Create the web server and listen on port 8080
-# server_address = ('', 8080)
-# httpd = HTTPServer(server_address, MyRequestHandler)
-# print('Server running at http://localhost:8080')
-#
-# # Start the server
-# httpd.serve_forever()
